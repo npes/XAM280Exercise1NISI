6598d8b6d60bebc5ee3a40a97eeeeb1eb1e71478
npes <npesdk@gmail.com>
2018-03-20 11:00:03 +0100

    Initial commit

---
+++ FunFlacts/AllFlags.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlagData;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagData
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlags : ContentPage
+    {
+        public AllFlags()
+        {
+            InitializeComponent();
+            flags.ItemsSource = DependencyService.Get<FunFlactsViewModel>().Flags;
+            BindingContext = DependencyService.Get<FunFlactsViewModel>();
+        }
+    }
+}
